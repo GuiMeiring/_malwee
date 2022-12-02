81e03040d23a856e5d66c73a31b30dd031a2d5f5
guilherme meiring <guilhermemeiring08@gmail.com>
2022-12-02 11:10:39 -0300

    arrumando Date

--- client1/src/app/modal-add-requests/modal-add-requests.component.ts
+++ client1/src/app/modal-add-requests/modal-add-requests.component.ts
@@ -12,8 +12,14 @@ export class ModalAddRequestsComponent implements OnInit {
   search: string ='';
   client : Array<any>= [];
   startDate : Date = new Date();
-  DateDelivery: Date= new Date();
+  DateDelivery: any;
   startdate : any;
+  dia: number | undefined;
+  nameClient: string ='';
+  fkClients: number | undefined;
+  address: Array<any>=[];
+  fkAddress: number | undefined;
+  rua: string='';
 
 
   constructor(private router : Router, private httpService: HttpService) { }
@@ -24,12 +30,12 @@ export class ModalAddRequestsComponent implements OnInit {
     console.log(dia);
     let mes =String(this.startDate.getMonth()+1).padStart(2,'0');
     console.log(mes);
-    const ano =this.startDate.getFullYear();
+    let ano =this.startDate.getFullYear();
     console.log(ano);
     this.startdate = (`${mes}/${dia}/${ano}`);
     this.startDate = this.startdate;
-    console.log(this.startDate);
-
+    this.dia = parseInt(dia)+7;
+    this.DateDelivery= new Date(`${mes}/${this.dia}/${ano}`);
   }
   async listaClients(){
     this.client= await this.httpService.get('client');
@@ -37,6 +43,22 @@ export class ModalAddRequestsComponent implements OnInit {
 
 
   }
+  public addNameClient(name: string, id: number){
+          this.nameClient=name;
+          this.fkClients=id;
+          this.listaAddress();
+      }
+      async listaAddress(){
+        this.address= await this.httpService.get(`client/${this.fkClients}`);
+        console.log(this.address);
+
+
+      }
+      public addAddress(rua: string, idAddress: number){
+        this.rua= rua;
+        this.fkAddress=idAddress;
+    }
+
 
 
 }
